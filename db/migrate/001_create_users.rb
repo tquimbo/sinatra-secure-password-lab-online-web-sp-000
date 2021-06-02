d90787d0ff24c21a5d5c74a245afcf6b58c07b5d
Tim Quimbo <tquimbo1@gmail.com>
2021-06-01 22:31:11 -0400

    Done.

---
+++ db/migrate/001_create_users.rb
@@ -0,0 +1,11 @@
+class CreateUsers
+
+    def change
+        create_table :users do |t|
+            t.string :username
+            t.string :password_digest
+        end
+      end
+    end
+
+end
