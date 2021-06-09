3fafcba0f57dd7b4f8ae5f667438f88eb79237e9
Tim Quimbo <tquimbo1@gmail.com>
2021-06-09 14:47:41 -0400

    Done.

--- db/migrate/001_create_users.rb
+++ db/migrate/001_create_users.rb
@@ -1,11 +1,8 @@
-class CreateUsers
-
+class CreateUsers < ActiveRecord::Migration[5.1]
     def change
-        create_table :users do |t|
-            t.string :username
-            t.string :password_digest
-        end
+      create_table :users do |t|
+        t.string :username
+        t.string :password_digest
       end
     end
-
-end
+  end
